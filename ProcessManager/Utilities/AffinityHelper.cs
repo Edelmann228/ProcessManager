676c2f38b8bdb4a95109ef0f87107e4ff35e8e4a
Edelmann228 <Faran17@mail.ru>
2026-02-28 02:15:38 +0300

    create class AffinityHelper

---
+++ ProcessManager/Utilities/AffinityHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ProcessManager.Utilities
+{
+    public static class AffinityHelper
+    {
+        public static IntPtr BuildMask(bool[] cores)
+        {
+            long mask = 0;
+            for (int i = 0; i < cores.Length; i++)
+                if (cores[i])
+                    mask |= (1L << i);
+
+            return new IntPtr(mask);
+        }
+
+        public static bool IsEnabled(IntPtr mask, int core)
+        {
+            return (mask.ToInt64() & (1L << core)) != 0;
+        }
+
+        public static string ToBinary(IntPtr mask)
+        {
+            long value = mask.ToInt64();
+            return Convert.ToString(value, 2).PadLeft(Environment.ProcessorCount, '0');
+        }
+
+        public static string ToHex(IntPtr mask)
+            => "0x" + mask.ToInt64().ToString("X");
+    }
+}
