eba0f3ff5849a1f2ffdb00597bd257e727de0ec4
Edelmann228 <Faran17@mail.ru>
2026-02-28 02:17:25 +0300

    Create UI

---
+++ ProcessManager/Views/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ProcessManager.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Дополнительная привязка команд
+            CommandBindings.Add(new CommandBinding(
+                new RoutedCommand("DeleteCommand", typeof(MainWindow)),
+                (s, e) => ExecuteDelete(),
+                (s, e) => e.CanExecute = (DataContext as ViewModels.MainViewModel)?.SelectedProcess != null
+            ));
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                var vm = DataContext as ViewModels.MainViewModel;
+                if (vm?.SelectedProcess != null)
+                {
+                    vm.KillCommand.Execute(null);
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void ExecuteDelete()
+        {
+            var vm = DataContext as ViewModels.MainViewModel;
+            vm?.KillCommand.Execute(null);
+        }
+    }
+}
