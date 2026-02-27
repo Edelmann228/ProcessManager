e4bae9fb7208e2a8061ca100f69f97a86bf6fa54
Edelmann228 <Faran17@mail.ru>
2026-02-28 02:13:43 +0300

    Cоздан класс ThreadInfo

---
+++ ProcessManager/Models/ThreadInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace ProcessManager.Models
+{
+    public class ThreadInfo
+    {
+        public int Id { get; set; }
+        public ThreadPriorityLevel Priority { get; set; }
+        public ThreadState State { get; set; }
+        public System.TimeSpan CpuTime { get; set; }
+    }
+}
